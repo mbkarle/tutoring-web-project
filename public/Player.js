e2dfd2074e5ccc0eee969ad27655140b67a68139
Matthew Karle <mkarle2178@gmail.com>
2020-05-13 00:42:44 -0400

    created star element in html

---
+++ public/Player.js
@@ -0,0 +1,10 @@
+//Just like in python, class created with "class className", but now use curly braces instead of colon as in JS
+class Player {
+    //in JavaScript, class methods do not need "function" in front of them, they are properties of the class
+    //methods also do not need to be given "self" as an input, instead always have access to "this" keyword which has same functionality
+
+    //constructor is the analogous function to __init__ in python
+    
+
+
+}
