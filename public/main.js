f95180456a266ddd9384e1f3e33b1def28ddfa23
Bjoern Norheim <bjoernnorheim@Bjoerns-MacBook-Pro.local>
2020-05-21 17:10:24 -0700

    spaceship movement

--- public/main.js
+++ public/main.js
@@ -1,5 +1,21 @@
 /*----------Main JS----------*/
+var keyAllowed= {
+  "w":true,
+  "s":true,
+  "a":true,
+  "d":true,
+}
+var timerID={
+  "w":-1,
+  's':-1,
+  'a':-1,
+  "d":-1,
 
+
+
+}
+var playerX = 0
+var playerY = 0
 window.onload = function() {
     //wait for dom to load
     console.log("Loaded");
@@ -11,6 +27,65 @@ var height =window.innerHeight
       createStar(i, randomInt(0, width), randomInt(0,height ), randomInt(15, 50), randomInt(1, 10))
 
     }
+// var x = 0
+// var y = 0
+// var movement = setInterval(function(){
+// var player =document.getElementById("player")
+// player.style.transform = ("translate(" + x + "px, " + y + "px)")
+// x += 1
+// y += 1
+//
+// }, 40)
+//
+// setTimeout(function(){
+// clearInterval(movement)
+//
+//
+// }, 10000)
+document.addEventListener("keydown", function(e){
+console.log(e.key)
+if(keyAllowed[e.key]== false){
+  return
+}
+if(e.key == "w"){
+  timerID["w"] = setInterval(function(){
+   playerY -= 2
+   move(playerX, playerY)
+
+  },45 )
+}
+if(e.key == "s"){
+  timerID["s"] = setInterval(function(){
+    playerY += 2
+    move(playerX, playerY)
+
+  }, 45)
+}
+if(e.key == "a"){
+  timerID["a"]= setInterval(function(){
+playerX -= 2
+move(playerX, playerY)
+
+
+  }, 45)
+}
+if(e.key == "d"){
+  timerID["d"]= setInterval(function(){
+playerX += 2
+move(playerX, playerY)
+
+  }, 45)
+}
+
+keyAllowed[e.key]= false
+})
+
+document.addEventListener("keyup", function(e){
+ keyAllowed[e.key] = true
+ clearInterval(timerID[e.key])
+})
+
+
 }
 
 function createStar(num, x, y, blur, spread){
@@ -23,7 +98,11 @@ function createStar(num, x, y, blur, spread){
   document.getElementById("starsSet").append(star)
 }
 
+function move(x, y){
+var player = document.getElementById("player")
+player.style.transform = ("translate(" + x + "px, " + y + "px)")
 
+}
 
 function randomInt(min, max){
 return(Math.floor(Math.random()*(max-min))+ min)
