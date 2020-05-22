ecbcf75e49f54be9855f36591a480c220c23120f
Bjoern Norheim <bjoernnorheim@Bjoerns-MacBook-Pro.local>
2020-05-21 18:35:32 -0700

    object orientation

--- public/Player.js
+++ public/Player.js
@@ -1,10 +1,95 @@
 //Just like in python, class created with "class className", but now use curly braces instead of colon as in JS
-class Player {
-    //in JavaScript, class methods do not need "function" in front of them, they are properties of the class
-    //methods also do not need to be given "self" as an input, instead always have access to "this" keyword which has same functionality
+console.log("Player linked in")
 
-    //constructor is the analogous function to __init__ in python
-    
+class Player {
+  constructor(health, damage, x, y, outsideID){
+    this.health = health
+    this.damage = damage
+    this.x = x
+    this.y = y
+    this.element = document.getElementById(outsideID)
+
+     this.timerID={
+      "w":-1,
+      's':-1,
+      'a':-1,
+      "d":-1
+    }
+    this.keyAllowed= {
+      "w":true,
+      "s":true,
+      "a":true,
+      "d":true
+    }
+
+
+  }
+draw(){
+    this.element.style.transform =("translate(" + this.x +"px, " + this.y +"px)")
+}
+
+
+shoot(){
+
+}
+keyPress(e){
+let self = this
+  if(!this.keyAllowed[e.key]){
+    return
+  }
+  if(e.key == "w"){
+    this.timerID["w"] = setInterval(function(){
+     self.y -= 2
+     self.draw()
+
+    },45 )
+  }
+  if(e.key == "s"){
+    this.timerID["s"] = setInterval(function(){
+      self.y += 2
+      self.draw()
+
+    }, 45)
+  }
+  if(e.key == "a"){
+    this.timerID["a"]= setInterval(function(){
+  self.x -= 2
+  self.draw()
+
+
+    }, 45)
+  }
+  if(e.key == "d"){
+    this.timerID["d"]= setInterval(function(){
+  self.x += 2
+  self.draw()
+
+   }, 45)
+  }
+
+  this.keyAllowed[e.key]= false
+
+
+
+  }
+keyUp(e){
+  if(!this.keyAllowed[e.key]){
+    this.keyAllowed[e.key] = true
+    clearInterval(this.timerID[e.key])
+  }
+
+}
+
+
 
 
 }
+
+
+
+
+
+
+//in JavaScript, class methods do not need "function" in front of them, they are properties of the class
+//methods also do not need to be given "self" as an input, instead always have access to "this" keyword which has same functionality
+//constructor is the analogous function to __init__ in python
--- public/main.js
+++ public/main.js
@@ -1,21 +1,4 @@
 /*----------Main JS----------*/
-var keyAllowed= {
-  "w":true,
-  "s":true,
-  "a":true,
-  "d":true,
-}
-var timerID={
-  "w":-1,
-  's':-1,
-  'a':-1,
-  "d":-1,
-
-
-
-}
-var playerX = 0
-var playerY = 0
 window.onload = function() {
     //wait for dom to load
     console.log("Loaded");
@@ -25,67 +8,12 @@ var height =window.innerHeight
 
     for(var i = 50; i > 0 ; i-- ){
       createStar(i, randomInt(0, width), randomInt(0,height ), randomInt(15, 50), randomInt(1, 10))
-
     }
-// var x = 0
-// var y = 0
-// var movement = setInterval(function(){
-// var player =document.getElementById("player")
-// player.style.transform = ("translate(" + x + "px, " + y + "px)")
-// x += 1
-// y += 1
-//
-// }, 40)
-//
-// setTimeout(function(){
-// clearInterval(movement)
-//
-//
-// }, 10000)
-document.addEventListener("keydown", function(e){
-console.log(e.key)
-if(keyAllowed[e.key]== false){
-  return
-}
-if(e.key == "w"){
-  timerID["w"] = setInterval(function(){
-   playerY -= 2
-   move(playerX, playerY)
+    var player = new Player(100, 20, 0, 0,"player" )
 
-  },45 )
-}
-if(e.key == "s"){
-  timerID["s"] = setInterval(function(){
-    playerY += 2
-    move(playerX, playerY)
+document.addEventListener("keydown", function(e){player.keyPress(e)})
 
-  }, 45)
-}
-if(e.key == "a"){
-  timerID["a"]= setInterval(function(){
-playerX -= 2
-move(playerX, playerY)
-
-
-  }, 45)
-}
-if(e.key == "d"){
-  timerID["d"]= setInterval(function(){
-playerX += 2
-move(playerX, playerY)
-
-  }, 45)
-}
-
-keyAllowed[e.key]= false
-})
-
-document.addEventListener("keyup", function(e){
- keyAllowed[e.key] = true
- clearInterval(timerID[e.key])
-})
-
-
+document.addEventListener("keyup", function(e){player.keyUp(e)})
 }
 
 function createStar(num, x, y, blur, spread){
@@ -93,19 +21,10 @@ function createStar(num, x, y, blur, spread){
   star.classList.add("star")
   star.id = "newStar" + num
   star.style.transform = ("translate(" + x + "px, " + y + "px)")
-  // document.getElementById("starsSet").append(star)
   star.style.boxShadow = ("0px 0px "+ blur + "px " + spread+ "px rgba(253, 253, 253, 0.8)")
   document.getElementById("starsSet").append(star)
 }
 
-function move(x, y){
-var player = document.getElementById("player")
-player.style.transform = ("translate(" + x + "px, " + y + "px)")
-
-}
-
 function randomInt(min, max){
 return(Math.floor(Math.random()*(max-min))+ min)
-
-
 }
